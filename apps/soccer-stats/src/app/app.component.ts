8cfe0a4c5d2228cb18fd2f3029fd26c08a555ae8
Paul Spears <dpsthree@gmail.com>
2021-01-14 17:01:55 -0600

    initial commit

---
+++ apps/soccer-stats/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'big-co-soccer-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'soccer-stats';
+}
