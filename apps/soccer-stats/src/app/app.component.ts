4bb20c683c77222fcd32935074614f4430a5801d
Paul Spears <dpsthree@gmail.com>
2021-01-14 19:58:34 -0600

    add logout button

--- apps/soccer-stats/src/app/app.component.ts
+++ apps/soccer-stats/src/app/app.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '@bsc/shared/data-access-auth';
+import { LogInOutService } from '@bsc/soccer-stats/data-access-log-in-out';
 
 @Component({
   selector: 'bsc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  currentUser = this.authenticationService.currentUser;
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private logInOut: LogInOutService
+  ) {}
+
+  logout() {
+    this.logInOut.logout();
+  }
+}
