cb7cf6d88d0a18c8bb97e0a1cf401c5b7c8938fc
Paul Spears <dpsthree@gmail.com>
2021-01-15 11:44:02 -0600

    translate the app component

--- apps/soccer-stats/src/app/app.module.ts
+++ apps/soccer-stats/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { SharedDataAccessAuthModule } from '@bsc/shared/data-access-auth';
 import { SharedDataAccessAuthzModule } from '@bsc/shared/data-access-authz';
-import { SharedUtilTranslationConfigModule} from '@bsc/shared/util-translation-config';
+import { SharedUtilTranslationConfigModule } from '@bsc/shared/util-translation-config';
 
 import { environment } from '../environments/environment';
 
@@ -14,10 +15,14 @@ import { AppRoutingModule } from './app-routing.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     SharedDataAccessAuthModule.forRoot(environment.authenticationConfig),
     SharedDataAccessAuthzModule.forRoot(environment.authorizationConfig),
-    SharedUtilTranslationConfigModule.forRoot(environment.production, './assets/i18n')
+    SharedUtilTranslationConfigModule.forRoot(
+      environment.production,
+      './assets/i18n'
+    )
   ],
   bootstrap: [AppComponent]
 })
