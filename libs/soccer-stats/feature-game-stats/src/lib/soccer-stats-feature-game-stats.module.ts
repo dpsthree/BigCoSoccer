cace1afbd665ab1b896a6f3b96c651254e93a46e
Paul Spears <dpsthree@gmail.com>
2021-01-14 18:22:44 -0600

    make the features lazy loadable

--- libs/soccer-stats/feature-game-stats/src/lib/soccer-stats-feature-game-stats.module.ts
+++ libs/soccer-stats/feature-game-stats/src/lib/soccer-stats-feature-game-stats.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { GameStatsPageComponent } from './game-stats-page/game-stats-page.component';
 
+const routes: Routes = [{ path: '', component: GameStatsPageComponent }];
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [GameStatsPageComponent]
 })
 export class SoccerStatsFeatureGameStatsModule {}
--- libs/soccer-stats/feature-player-stats/src/lib/soccer-stats-feature-player-stats.module.ts
+++ libs/soccer-stats/feature-player-stats/src/lib/soccer-stats-feature-player-stats.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
 import { PlayerStatsPageComponent } from './player-stats-page/player-stats-page.component';
 
+const routes: Routes = [{ path: '', component: PlayerStatsPageComponent }];
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule.forChild(routes)],
   declarations: [PlayerStatsPageComponent]
 })
 export class SoccerStatsFeaturePlayerStatsModule {}
