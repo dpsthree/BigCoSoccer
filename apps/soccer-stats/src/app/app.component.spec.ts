8adae7f2d0ca10036b4c962ce5f16235bb476442
Paul Spears <dpsthree@gmail.com>
2021-01-18 13:56:46 -0600

    fix unit tests

--- apps/soccer-stats/src/app/app.component.spec.ts
+++ apps/soccer-stats/src/app/app.component.spec.ts
@@ -1,5 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { SharedDataAccessAuthModule } from '@bsc/shared/data-access-auth';
+import { SharedDataAccessAuthzModule } from '@bsc/shared/data-access-authz';
+import { SharedUtilTranslationConfigModule } from '@bsc/shared/util-translation-config';
+
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 
 // Test the ability to create the component
@@ -7,7 +15,18 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([])],
+      imports: [
+        // We will eventually want to make
+        // the following line disappear
+        HttpClientModule,
+        SharedDataAccessAuthModule.forRoot(environment.authenticationConfig),
+        SharedDataAccessAuthzModule.forRoot(environment.authorizationConfig),
+        RouterTestingModule.withRoutes([]),
+        SharedUtilTranslationConfigModule.forRoot(
+          environment.production,
+          './assets/i18n'
+        )
+      ],
       declarations: [AppComponent]
     }).compileComponents();
   });
